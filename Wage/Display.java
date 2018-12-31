b594f87df2b9c443703e688d452557cdef074b4f
wang0561 <wang0561@algonquinlive.com>
2018-12-31 11:04:55 -0500

    Update Display.java

--- Wage/Display.java
+++ Wage/Display.java
@@ -1,241 +1,200 @@
-package cst8110.game;
+package wage.display.javafx;
+/*
+ * 
+ * using "Extract required libraries in to generated jar file"
+ * */
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.io.*;
 
-import static java.lang.System.*;
-import java.util.*;
+public class Display extends Application {
 
-public class Prototype {
-	public static void main(String args[]) {
-		new Play().doGame();
+	private final static Label Hlabel = new Label("Enter Hours: ");
+	private final static Label Rlabel = new Label("Enter Rates: ");
+	private final static Label Nlabel = new Label("Enter Name: ");
+	private final static Label Alabel = new Label("Enter Age: ");
+	private final static Label Glabel = new Label("Gender : ");
+	private final static ObservableList<String> options = FXCollections.observableArrayList("Male", "Female", "Other");
+	private static final ComboBox<String> Gender = new ComboBox<String>(options);
+	private TextField nField = new TextField();
+	private TextField hField = new TextField();
+	private TextField rField = new TextField();
+	private TextField aField = new TextField();
+	private GridPane pane1 = new GridPane();
+	private static Button button = new Button("Calculate");
+
+	private String name, gender;
+	private double hour, rate;
+	private int age;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
 	}
 
-}
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		button.setOnAction(getHandler());
+		pane1.setHgap(10);
+		pane1.setVgap(10);
 
-class Play {
-	private static final String WELCOME = "Welcome to Dice Roulette Deluxe ";
-	private static final String ASKDICE = "How many dice do you want?";
-	private static final String INVALIDDICE = "You need at least one die";
-	private static final String ASKSIDE = "How many sides do they have?";
-	private static final String INVALIDSIDE = "It needs to have at least 2 sides";
-	private static final String YOURPOT = "Your pot is: $";
-	private static final String ASKBET = "How much do you want to bet?";
-	private static final String INVALIDBET = "Invalid bet, pick a value between 0 and ";
-	private static final String ASKCHOICE = "What is your choice?";
-	private static final String INVALIDCHOICE = "Invalid choice, choose odd, even, high, or low";
-	private static final String YOUROLLED = "You rolled:";
-	private static final String TOTAL = "total=";
-	private static final String WIN = "You win";
-	private static final String LOSE = "You lose";
-	public static final String THANKS = "Thank you for playing";
-	private static final String AGAIN = "Do the game again?";
-	private Scanner input = new Scanner(in);
-	private int dice, side, total;
-	private List<Integer> list;
-	private String choice;
-	private boolean result;
-	private Game game;
-	private final static double startPot = 110.0;
-	private double currentPot, bet;
+		pane1.add(Nlabel, 0, 0);
+		pane1.add(Glabel, 0, 1);
+		pane1.add(Alabel, 0, 2);
+		pane1.add(Hlabel, 0, 3);
+		pane1.add(Rlabel, 0, 4);
+		pane1.add(button, 0, 5);
 
-	public void doGame() {
-		newGame();
-		do {
-			do {
-				askBet();
-				if (betZero()) {
-					out.println(THANKS);
-					break;
+		pane1.add(nField, 1, 0);
+		pane1.add(Gender, 1, 1);
+		pane1.add(aField, 1, 2);
+		pane1.add(hField, 1, 3);
+		pane1.add(rField, 1, 4);
+
+		Scene scene = new Scene(pane1, 400, 400);
+		stage = new Stage();
+		stage.setTitle("Enter the Information form:");
+		stage.setScene(scene);
+		stage.show();
+
+	}
+
+	private EventHandler<ActionEvent> getHandler() {
+		// TODO Auto-generated method stub
+		return new EventHandler<ActionEvent>() {
+			StringBuilder sb = new StringBuilder();
+			Person person;
+			Wage wage;
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				initData();
+				if (checkData()) {
+
+					wage = new Wage(hour, rate);
+					person = new Person(wage, name, gender, age);
+					Stage stage = new Stage();
+
+					Text text = new Text(person.toString());
+					Button button = new Button("Export");
+					button.setOnAction(wirteToFile());
+					VBox vb = new VBox();
+					vb.getChildren().add(text);
+					vb.getChildren().add(button);
+
+					Scene scene = new Scene(vb, 400, 300);
+					stage.setScene(scene);
+					stage.setTitle("Result ");
+					stage.show();
+				} else {
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Invalid input");
+					alert.setContentText("Invalid input, please correct the " + sb.toString());
+					sb = new StringBuilder();
+					alert.showAndWait();
 				}
-				askChoice();
-				output();
+			}
 
-			} while (getCurrentPot() > 0);
-			if (betZero())
-				break;
-		} while (doAgain());
+			private EventHandler<ActionEvent> wirteToFile() {
+				// TODO Auto-generated method stub
+				return new EventHandler<ActionEvent>() {
+
+					@Override
+					public void handle(ActionEvent arg0) {
+						// TODO Auto-generated method stub
+						String str = person.toString();
+						File file = new File(name + ".txt");
+						FileOutputStream fis = null;
+						/*
+						 * if write into excel/csv file, use StringBuider and PrintWriter instead.
+						 * 
+						 * */
+						try {
+							if (!file.exists())
+								file.createNewFile();
+							fis = new FileOutputStream(file);
+							byte[] bytes = str.getBytes();
+							fis.write(bytes, 0, bytes.length);
+						} catch (IOException e) {
+							
+							Alert alert = new Alert(AlertType.ERROR);
+							alert.setTitle("Failed");
+							alert.setContentText(
+									"cannot export the data to the file " + file.getName() + e.getMessage());
+							
+							alert.showAndWait();
+						}finally {
+							try {
+								fis.close();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+
+						Alert alert = new Alert(AlertType.INFORMATION);
+						alert.setTitle("SUCCESSFUL");
+						alert.setContentText("The file " + file.getName() + "has been created.");
+						sb = new StringBuilder();
+						alert.showAndWait();
+					}
+
+				};
+			}
+
+			private boolean checkData() {
+				boolean correct = true;
+				if (name.equals("")) {
+					correct = false;
+					sb.append("name,");
+				}
+				if (age == 0) {
+					correct = false;
+					sb.append("age,");
+				}
+				if (gender == null) {
+					correct = false;
+					sb.append("gender,");
+				}
+				if (hour == 0) {
+					correct = false;
+					sb.append("hour,");
+				}
+				if (rate == 0) {
+					correct = false;
+					sb.append("rate,");
+				}
+
+				return correct;
+			}
+		};
 	}
 
-	public Play() {
-	}
+	private void initData() {
 
-	private void print(String s) {
-		out.println(s);
-	}
+		name = nField.getText();
+		gender = Gender.getValue();
 
-	public double getBet() {
-		return bet;
-	}
+		try {
+			age = Integer.parseInt(aField.getText());
+			hour = Double.parseDouble(hField.getText());
+			rate = Double.parseDouble(rField.getText());
 
-	public double getCurrentPot() {
-		return currentPot;
-
-	}
-
-	
-	public void newGame() {
-		// ask dice and sides
-		print(ASKDICE);
-		dice = input.nextInt();
-		verifyDice(dice);
-		print(ASKSIDE);
-		side = input.nextInt();
-		verifySide(side);
-		game = new Game(dice, side);
-
-		print(WELCOME + "\n\n");
-
-	}
-
-	public boolean betZero() {
-		return bet == 0;
-	}
-	// begain the logic of game
-
-	public void askBet() {
-		// ask bet and choice
-		if (currentPot == 0)
-			currentPot = startPot;
-		print(YOURPOT + currentPot);
-		print(ASKBET);
-		bet = Double.parseDouble(input.next());
-		verifyBet(bet);
-
-	}
-
-	public void askChoice() {
-		print(ASKCHOICE);
-		choice = input.next();
-		verifyChoice(choice);
-	}
-
-	public void output() {
-		list = game.getRoll();
-		print(YOUROLLED + list);
-		print(TOTAL + game.getTotal());
-		if (winOrLose()) {
-			print(WIN);
-			currentPot += bet;
-		} else {
-			print(LOSE);
-			currentPot -= bet;
-		}
-	}
-
-	public boolean doAgain() {
-		print(AGAIN);
-		String yesorno = input.next();
-		if ("y".equalsIgnoreCase(yesorno))
-			return true;
-		print(THANKS);
-		return false;
-	}
-
-	private boolean winOrLose() {
-		result = false;
-		int isHigh = dice * (side + 1) / 2;
-		total = game.getTotal();
-		switch (choice) {
-		case "low":
-			if (total < isHigh)
-				result = true;
-			break;
-		case "high":
-			if (total >= isHigh)
-				result = true;
-			break;
-		case "odd":
-			if (total % 2 != 0)
-				result = true;
-			break;
-		case "even":
-			if (total % 2 == 0)
-				result = true;
-			break;
-		default:
-			break;
-		}
-		return result;
-	}
-
-	private void verifyChoice(String s) {
-		while (!correctChoice(s)) {
-			print(INVALIDCHOICE);
-			s = input.next();
+		} catch (Exception e) {
 
 		}
-		this.choice = s;
-	}
 
-	private boolean correctChoice(String s) {
-		return "low".equals(s) || "high".equals(s) || "odd".equals(s) || "even".equals(s);
-	}
-
-	private void verifyBet(double be) {
-		while (be < 0 || be > currentPot) {
-			print(INVALIDBET + currentPot);
-			be = input.nextDouble();
-		}
-		this.bet = be;
-	}
-
-	private void verifySide(int side) {
-		while (side < 2) {
-			print(INVALIDSIDE);
-			print(ASKSIDE);
-			side = input.nextInt();
-
-		}
-		this.side = side;
-	}
-
-	private void verifyDice(int dice) {
-		while (dice < 1) {
-			print(INVALIDDICE);
-			print(ASKDICE);
-			dice = input.nextInt();
-
-		}
-		this.dice = dice;
-	}
-
-}
-
-class Game {
-	private static Random r = new Random();
-	private int dice;
-	private int side;
-	private ArrayList<Integer> roll;
-
-	public Game() {
-	}
-
-	public Game(int dice, int side) {
-
-		setDice(dice);
-		setSide(side);
-
-	}
-
-	public void setDice(int dice) {
-		this.dice = dice;
-	}
-
-	public void setSide(int side) {
-		this.side = side;
-	}
-
-	public List<Integer> getRoll() {
-		roll = new ArrayList<>();
-		for (int i = 0; i < dice; i++)
-			roll.add(r.nextInt(side) + 1);
-		return roll;
-	}
-
-	public int getTotal() {
-		int sum = 0;
-		if (roll != null) {
-			for (int i : roll)
-				sum += i;
-		}
-		return sum;
 	}
 }
